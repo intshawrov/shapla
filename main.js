8fb6fb49b5e0214e0e7779fd05388c7cea7b44dc
Shawrov <intshawrov@gmail.com>
2025-12-13 16:01:28 +0600

    updated

--- main.js
+++ main.js
@@ -6,3 +6,9 @@ const box = document.querySelector('#box');
 box.style.backgroundColor = 'green';
 box.style.width = '100px';
 box.style.height = '100px';
+
+document.getElementById('tom').style.background = 'blue';
+document.getElementById('tom').style.padding = '20px 10px';
+document.getElementById('tom').style.width = '200px'
+document.getElementById('tom').style.color = '#ffff';
+document.getElementById('tom').style.margin = '0 0 5px 0';
