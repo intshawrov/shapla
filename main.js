cb3ae6f4e354c9db4d2486f7ed4c9caba5c16837
Shawrov <intshawrov@gmail.com>
2025-12-13 17:16:40 +0600

    updated

--- main.js
+++ main.js
@@ -1,31 +1,73 @@
 "use strict";
 
-document.querySelector('h1').innerText = "Shawrov";
+const body = document.getElementById('body');
+const button = document.querySelectorAll('li');
 
-const box = document.querySelector('#box');
-box.style.backgroundColor = 'green';
-box.style.width = '100px';
-box.style.height = '100px';
+const redColor = document.querySelector('.red');
+redColor.addEventListener
 
-document.getElementById('tom').style.background = 'blue';
-document.getElementById('tom').style.padding = '20px 10px';
-document.getElementById('tom').style.width = '200px'
-document.getElementById('tom').style.color = '#ffff';
-document.getElementById('tom').style.margin = '0 0 5px 0';
 
-const menuItem = document.querySelectorAll('a');
-menuItem.forEach(function(i){
-      i.style.color = 'red';
-      i.style.fontSize = '20px';
-      // i.style.background = 'black';
-});
+const green = document.querySelector('.green');
+const blue = document.querySelector('.blue');
+const orange = document.querySelector('.orange');
 
-const cusClass = document.getElementsByClassName('nav-link');
 
-const convertedClass = Array.from(cusClass);
 
-convertedClass.forEach(function(i){
-      i.style.backgroundColor = '#1f923cff';
-      i.style.padding = '10px';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// document.querySelector('h1').innerText = "Shawrov";
+
+// const box = document.querySelector('#box');
+// box.style.backgroundColor = 'green';
+// box.style.width = '100px';
+// box.style.height = '100px';
+
+// document.getElementById('tom').style.background = 'blue';
+// document.getElementById('tom').style.padding = '20px 10px';
+// document.getElementById('tom').style.width = '200px'
+// document.getElementById('tom').style.color = '#ffff';
+// document.getElementById('tom').style.margin = '0 0 5px 0';
+
+// const menuItem = document.querySelectorAll('a');
+// menuItem.forEach(function(i){
+//       i.style.color = 'red';
+//       i.style.fontSize = '20px';
+//       // i.style.background = 'black';
+// });
+
+// const cusClass = document.getElementsByClassName('nav-link');
+
+// const convertedClass = Array.from(cusClass);
+
+// convertedClass.forEach(function(i){
+//       i.style.backgroundColor = '#1f923cff';
+//       i.style.padding = '10px';
       
-})
+// })
+
+
