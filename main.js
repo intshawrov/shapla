b083de53dab6acfc13092a82cdda42af3367cd50
Shawrov <intshawrov@gmail.com>
2025-12-13 15:55:40 +0600

    updated

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 "use strict";
 
 document.querySelector('h1').innerText = "Shawrov";
+
+const box = document.querySelector('#box');
+box.style.backgroundColor = 'green';
+box.style.width = '100px';
+box.style.height = '100px';
